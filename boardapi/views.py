113711b7c2fe450e2743c294ae982d8d0056a7b7
Roshan Maharjan <uid.sawyer@gmail.com>
2022-06-01 19:41:20 +0545

    Added BoardListCreate View and BoardSerializer

--- boardapi/views.py
+++ boardapi/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .models import Board
+from .serializers import BoardSerializer
+
+
+class BoardListCreate(APIView):
+    """
+    View to list all boards and create a new board in the system.
+    """
+
+    def get(self, request):
+        """
+        Return a list of all boards
+        """
+        boards = Board.objects.all()
+        serializer = BoardSerializer(boards, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        """
+        Create a new board in the system
+        """
+        serializer = BoardSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
